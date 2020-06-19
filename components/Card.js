0454db2017807de133bbf209ff96cfb49665c8dd
Fulwani <satish.fulwani@accenture.com>
2020-06-19 14:03:08 +0530

    Add Validation and Alert for entered number.

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
   );
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,19 +1,86 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  Alert,
+} from "react-native";
+import Card from "../components/Card";
+import Input from "../components/Input";
+import Colors from "../constants/colors";
 
 export default function StartGameScreen(props) {
+  const [enteredNumber, setEnteredNumber] = useState("");
+  const [hasConfirmed, setHasConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const enteredNumberHandler = (inputNumber) => {
+    setEnteredNumber(inputNumber.replace(/[^0-9]/, ""));
+  };
+
+  const resetInputHandler = () => {
+    setEnteredNumber("");
+    setHasConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid number!", "Number has to be between 1 and 99.", [
+        { text: "Okay", style: "destructive", onPress: resetInputHandler },
+      ]);
+      return;
+    }
+    setHasConfirmed(true);
+    setEnteredNumber("");
+    setSelectedNumber(parseInt(enteredNumber));
+  };
+  let confirmedOutput;
+  if (hasConfirmed) {
+    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+  }
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>The Game Screen</Text>
-      <View style={styles.inputContainer}>
-        <Text>Select a Number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
-        </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>The Game Screen</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCorrect={false}
+            keyboardType="numeric"
+            maxLength={2}
+            value={enteredNumber}
+            onChangeText={enteredNumberHandler}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Reset"
+                onPress={() => resetInputHandler()}
+                color={Colors.accent}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={() => confirmInputHandler()}
+                color={Colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+        {confirmedOutput}
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -31,19 +98,18 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 8,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
     width: "100%",
     justifyContent: "space-between",
     paddingHorizontal: 15,
+  },
+  input: {
+    width: 30,
+    textAlign: "center",
+  },
+  button: {
+    width: "40%",
   },
 });
